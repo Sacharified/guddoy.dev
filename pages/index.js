63e219b3a231bcfc6e06a7f73a86aaa7e32bc395
Sacha <sacha@grabyo.com>
2019-07-06 12:42:19 +0100

    add netlify tag

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ function Home() {
                 <Typography variant="h4" component="h4" gutterBottom>
                     Get in touch
                 </Typography>
-                <Form name="contact" method="POST" netlify="true">
+                <Form name="contact" method="POST" netlify>
                     <InputContainer>
                         <TextField
                             id="name"
