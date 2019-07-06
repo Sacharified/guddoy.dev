07779ca396a709374002082da3d751818783c4c7
Sacha <sacha@grabyo.com>
2019-07-06 12:37:10 +0100

    add form styling

--- components/form.js
+++ components/form.js
@@ -1,18 +1,48 @@
-import Button from "@material-ui/core/Button"
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => {
+    const verticalSpacing = {
+        marginBottom: `${theme.spacing(2.5)}px`
+    };
+    return {
+        root: {
+            backgroundColor: theme.palette.grey[50],
+            padding: `${theme.spacing(2.5)}px`
+        },
+        button: {
+        },
+        inputContainer: {
+            ...verticalSpacing
+        }
+    }
+});
+
 const FormComponent = props => {
+    const classes = useStyles();
     return (
-        <form {...props}>
+        <form {...props} className={classes.root}>
             {props.children}
             <div>
                 <Button
                     variant="contained"
                     color="primary"
                     type="submit"
+                    className={classes.button}
                 >
                     Submit
                 </Button>
             </div>
         </form>
+    );
+};
+
+export const InputContainer = props => {
+    const classes = useStyles();
+    return (
+        <div className={classes.inputContainer}>
+            {props.children}
+        </div>
     );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
 import Head from "next/head";
-import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "components/iconbutton";
 import GithubIcon from "static/img/github-circle.svg";
 import TwitterIcon from "static/img/twitter.svg";
 import TextField from '@material-ui/core/TextField';
-import Form from "components/form";
-
-const handleSubmit = values => {
-    console.log(values);
-};
+import Form, { InputContainer } from "components/form";
 
 function Home() {
     return (
@@ -20,11 +15,11 @@ function Home() {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
             </Head>
-            <Container maxWidth="lg">
+            <Container maxWidth="sm">
                 <IconButton icon={GithubIcon} onClick={() => window.open(`https://github.com/Sacharified/guddoy.dev`)} />
                 <IconButton icon={TwitterIcon} onClick={() => window.open(`https://twitter.com/Sacharified`)} />
             </ Container>
-            <Container maxWidth="lg">
+            <Container maxWidth="sm">
                 <Typography variant="h2" component="h2" gutterBottom>
                     Sacha Guddoy
                 </Typography>
@@ -32,16 +27,43 @@ function Home() {
                     Web developer <a href="https://about.grabyo.com" target="_blank">@grabyo</a>
                 </Typography>
             </Container>
-            <Container maxWidth="lg">
+            <Container maxWidth="sm">
                 <Typography variant="h4" component="h4" gutterBottom>
                     Get in touch
-                    <Form name="contact" method="POST" netlify="true" onSubmit={handleSubmit}>
+                </Typography>
+                <Form name="contact" method="POST" netlify="true">
+                    <InputContainer>
                         <TextField
                             id="name"
-                            placeholder="name"
+                            name="name"
+                            label="Name"
+                            required
+                            fullWidth
                         />
-                    </Form>
-                </Typography>
+                    </InputContainer>
+                    <InputContainer>
+                        <TextField
+                            id="name"
+                            name="email"
+                            label="Email Address"
+                            type="email"
+                            required
+                            fullWidth
+                        />
+                    </InputContainer>
+                    <InputContainer>
+                        <TextField
+                            id="name"
+                            name="message"
+                            label="Message"
+                            required
+                            multiline
+                            rows="3"
+                            rowsMax="10"
+                            fullWidth
+                        />
+                    </InputContainer>
+                </Form>
             </Container>
         </>
     );
