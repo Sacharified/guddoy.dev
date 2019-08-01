85202b177fd5a9483e4899b4016e10ef5036e2a3
Sacha <sacha@grabyo.com>
2019-08-01 22:58:24 +0100

    fix test timezone

--- src/utils/date.test.js
+++ src/utils/date.test.js
@@ -3,8 +3,8 @@ import { formatDate } from "./date";
 describe(`Date Utils`, () => {
     describe(`formatDate()`, () => {
         test(`Formats the date`, () => {
-            const date = new Date("2019-08-01T21:43:00.845Z");
-            expect(formatDate(date)).toEqual("Thursday, August 1st, 2019, 10:43:00 PM");
+            const date = new Date("2019-08-01T21:43:00.845");
+            expect(formatDate(date)).toEqual("Thursday, August 1st, 2019, 9:43:00 PM");
         });
     });
 });
