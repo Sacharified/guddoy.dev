6dd53354d98b04dcdee3f24fe201f9d1ba5dc5ab
Sacha <sacha@grabyo.com>
2019-08-02 11:18:25 +0100

    merge branch 'develop'

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,16 +1,15 @@
 /// <reference types="Cypress" />
 
-context('Actions', () => {
+context("Actions", () => {
     beforeEach(() => {
-      cy.visit('/');
+      cy.visit("/");
     });
 
-    describe('Implicit Assertions', () => {
-        it('.should() - make an assertion about the current subject', () => {
-          // https://on.cypress.io/should
-          cy.get('.layout')
-            .find('h2')
-            .should('have.text', 'Sacha Guddoy')
+    describe("Implicit Assertions", () => {
+        it(".should() - make an assertion about the current subject", () => {
+          cy.get(".layout")
+            .find("h2")
+            .should("have.text", "Sacha Guddoy")
         });
     });
 });
