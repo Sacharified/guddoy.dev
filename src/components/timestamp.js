1817f70f2af528bf26918cb15843ae9001efa525
Sacha <sacha@grabyo.com>
2019-11-09 11:30:35 +0000

    Update timestamp component text

--- src/components/timestamp.js
+++ src/components/timestamp.js
@@ -2,8 +2,8 @@ import Typography from "@material-ui/core/Typography";
 import { formatDate } from "utils/date";
 
 const TimeStamp = ({ date }) => (
-  <Typography variant="caption">
-        Last updated: {formatDate(date)}
-  </Typography>
+	<Typography variant="caption">
+		Posted: {formatDate(date)}
+	</Typography>
 );
 export default TimeStamp;
